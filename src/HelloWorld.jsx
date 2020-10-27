0a1b2ea50ead9988397d68bf552c969140b5e33f
Carlos Bentz <carlos.bentz@hotmail.com>
2020-10-27 13:01:58 -0300

    finished

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import "./HelloWorld.css";
+
+function HelloWorld() {
+  return (
+    <div className="welcome">
+      <h1 className="title">Welcome peassant !</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
